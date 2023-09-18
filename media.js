2a3b96fc1e293a9c1dd82790b55dc45a23877ff1
marias06 <126470095+marias06@users.noreply.github.com>
2023-09-18 08:14:45 -0300

    18/09

---
+++ media.js
@@ -0,0 +1,5 @@
+// Cria referência ao form e elementos de resposta do progarma
+
+const form = document.querySelector("form")
+const resp1 = document.querySelector("h3")
+const resp2 = document.querySelector("h4")
