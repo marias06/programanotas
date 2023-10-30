eec94393d871fc5130dff9ff0440c9715c301884
marias06 <126470095+marias06@users.noreply.github.com>
2023-10-30 08:20:16 -0300

    30/10

---
+++ conta.js
@@ -0,0 +1,28 @@
+const form = document.querySelector("form")   //obtém elementos da página
+const resp1 = document.querySelector("#outResp1")
+const resp2 = document.querySelector("#outResp2")
+
+let numContas = 0     //declara e inicializa contador....
+let valTotal = 0     // ... e acumulador (variáveis globais)
+let resposta = ""    //string com a resposta a ser exibida
+
+//ouvinte
+
+form.addEventListener("submit", (e) =>{       //"escuta" evento submit do form
+    e.preventDefault()                        //evita envio do form
+    
+    const descricao = form.inDescrição.value   //obtém dados da conta
+    const valor = Number(form.inValor.value)
+
+    numContas++                          //adiciona valores ao contador e acumulador
+    valTotal = valTotal + valor
+
+    resposta = resposta + descricao + " -R$: " + valor.toFixed(2) + "\n"
+
+    resp1.innerText = `${resposta}-------------------------------------------------`
+    resp2.innerText = `${numContas} conta(s) - Total R$: ${valTotal.toFixed(2)}`
+
+    form.inDescrição.value = ""                   //limpa campos do form
+    form.inValor.value = ""
+    form.inDescrição.focus()                 //posiciona no campo inDescrição do form
+})
